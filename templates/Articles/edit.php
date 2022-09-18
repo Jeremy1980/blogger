516e3d26ceaf023daa3afa62481b705ae2c1b48f
Jarema <jeremy.cz@wp.pl>
2022-09-18 22:24:31 +0200

    Iteration 09.18

--- templates/Articles/edit.php
+++ templates/Articles/edit.php
@@ -3,8 +3,18 @@
     echo $this->Form->create($article);
     echo $this->Form->control('user_id', ['type' => 'hidden']);
     echo $this->Form->control('title');
-    echo $this->Form->control('body', ['rows' => '3']);
+    echo $this->Form->control('body', ['style' => 'height:300px !important;']);
     echo $this->Form->control('active', ['type' => 'checkbox' ,'checked'=> ($article->activated ?1 :0) ]);
     echo $this->Form->button(__('Save Article'));
     echo $this->Form->end();
 ?>
+<script type="text/javascript">
+ bkLib.onDomLoaded(function() { nicEditors.allTextAreas({
+     iconsPath : '/img/nicEditorIcons.gif'
+    ,buttonList : ['fontSize','indent','outdent','bold','italic','underline','strikeThrough','subscript','superscript'
+                    ,'left','center','right','justify','ol','ul',
+                    ,'image','link','unlink','hr']
+    ,maxHeight : 310
+    });
+  });
+</script>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -18,17 +18,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title><?= $this->fetch('title') ?></title>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $this->fetch('title') ?>
-    </title>
     <?= $this->Html->meta('icon') ?>
-
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
     <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'bootstrap.min', 'bootstrap-icons']) ?>
-    <?= $this->Html->script(['bootstrap.bundle.min']) ?>
+    <?= $this->Html->script(['bootstrap.bundle.min','nicEdit']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
